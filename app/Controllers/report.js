8f0265ab419460801fc676b5b29337a41ea9d3ee
abhi <abhivendrasingh@gmail.com>
2022-05-26 02:03:04 +0530

    added ejs support

--- app/Controllers/report.js
+++ app/Controllers/report.js
@@ -1,43 +1,63 @@
 import ReportService from '../Services/reportPrint';
 import Controller from './controller';
+const fs = require('fs');
+import ejs from 'ejs';
 const { performance } = require('perf_hooks');
 
+const ReportSettings = {
+  dailybalance: {
+    orientation: 'potrait',
+    format: 'A4',
+  }
+}
 export default class AccountController extends Controller {
-    constructor(response) {
-      super(response);
-      this.service = new ReportService();
-    }
-    async printOutstanding (request) {
-      try{  
-          const { format, orientation} = request.body;
-          var args = {"file":"OutstandingReport","format":format,"orientation":orientation}
-          const addUser =  await this.service.printReport(args)
-          this.sendResponse(addUser);   
-      } catch (error) {
-          this.handleException(error)
+  constructor(response) {
+    super(response);
+    this.service = new ReportService();
+  }
+
+  async printOutstanding(request) {
+    try {
+      const settings = ReportSettings.dailybalance;
+      const { data } = request.body;
+      const htmlData = await fs.readFile('../Resource/Dailybalance.ejs', { encoding: 'utf8' });
+      let html = ejs.render(htmlData, data);
+      var printOptions = {
+        "file": "OutstandingReport",
+        "format": settings.format,
+        "orientation": settings.orientation
       }
+      //fetch html file text
+
+      const addUser = await this.service.printReport(html, printOptions)
+      this.sendResponse(addUser);
+    } catch (error) {
+      this.handleException(error)
     }
-    async printFeeledger (request) {
-          try{     
-            const { format, orientation} = request.body;
-            var args = {"file":"FeeLedgerReport","format":format,"orientation":orientation}
-            const addUser =  await this.service.printReport(args)
-            this.sendResponse(addUser);     
-      } catch (error) {
-          this.handleException(error)
-      }
+  }
+
+  async printFeeledger(request) {
+    try {
+      const { format, orientation } = request.body;
+      var args = { "file": "FeeLedgerReport", "format": format, "orientation": orientation }
+      const addUser = await this.service.printReport(args)
+      this.sendResponse(addUser);
+    } catch (error) {
+      this.handleException(error)
     }
-     async printDailybalance (request) {
-      try{
-        const { format, orientation} = request.body;
-        var args = {"file":"Dailybalance","format":format,"orientation":orientation}
-        const addUser = await this.service.printReport(args)
-        this.sendResponse(addUser); 
-      } catch (error) {
-        console.log("sdds")
-        this.handleException(error)
-      }
+  }
+
+  async printDailybalance(request) {
+    try {
+      const { format, orientation } = request.body;
+      var args = { "file": "Dailybalance", "format": format, "orientation": orientation }
+      const addUser = await this.service.printReport(args)
+      this.sendResponse(addUser);
+    } catch (error) {
+      console.log("sdds")
+      this.handleException(error)
     }
+  }
 }
 
 
--- app/Services/reportPrint.js
+++ app/Services/reportPrint.js
@@ -6,77 +6,76 @@ var options = {
 };
 const { promisify } = require('util')
 const unlinkAsync = promisify(fs.unlink)
-var clouud= require('cloudinary').v2;
+var clouud = require('cloudinary').v2;
 var dateTime = require('node-datetime');
 import GroupModel from "../Model/ReportModel";
 import Database from "../Database-interaction/ReportRepositroy";
 import * as STATUS from "../Constants/StatusEnum";
 
-export default class AccountService{
-      constructor() {
-        this.repository = new Database();
+export default class AccountService {
+  constructor() {
+    this.repository = new Database();
+  }
+  async fileuploader(args, groupModel) {
+    try {
+      clouud.config({
+        cloud_name: process.env.cloudinary_cloud_name,
+        api_key: process.env.cloudinary_api_key,
+        api_secret: process.env.cloudinary_api_secret
+      })
+      groupModel['uploadingStartAt'] = this.bringTime()
+      await this.repository.updateReport(groupModel)
+      let focc = await clouud.uploader.upload(args, {
+        folder: `memes/posts`,
+        use_filename: true
+      })
+      groupModel['status'] = STATUS.STATUS.UPLOADED
+      groupModel['uploadingCompletedAt'] = this.bringTime()
+      groupModel['link'] = focc.secure_url
+      await this.repository.updateReport(groupModel)
+      return focc.secure_url
+    } catch (error) {
+      groupModel['status'] = STATUS.STATUS.FAILED_AT_UPLOADED
+      throw error;
     }
-    async fileuploader(args,groupModel){
-        try{
-          clouud.config({
-            cloud_name:process.env.cloudinary_cloud_name,
-            api_key:process.env.cloudinary_api_key,
-            api_secret:process.env.cloudinary_api_secret
-          })
-          groupModel['uploadingStartAt'] =this.bringTime()  
+  }
+  bringTime() {
+    var dt = dateTime.create();
+    var started = dt.format('Y-m-d H:M:S'); // YYYY-MM-DDTHH:MM:SS epoc time
+    return started
+  }
+  async printReport(html, options) {
+    try {
+      const groupModel = new GroupModel({ startedAt: this.bringTime(), status: STATUS.STATUS.STARTED });
+      await this.repository.createReport(groupModel)
+      groupModel['creationStartAt'] = this.bringTime()
+      pdf.create(html, options).toStream(async (err, pdfStream) => {
+        if (err) {
+          console.log(err)
+        } else {
+          groupModel['status'] = STATUS.STATUS.CREATED
+          groupModel['creationCompletedAt'] = this.bringTime()
           await this.repository.updateReport(groupModel)
-          let  focc = await clouud.uploader.upload(args,{
-                      folder: `memes/posts`,
-                      use_filename: true
+          const path = `./Print/${options.file}.pdf`;
+          const filePath = fs.createWriteStream(path);
+          pdfStream.pipe(filePath);
+          pdfStream.on('close', async () => {
+            try {
+              filePath.close();
+              let link = await this.fileuploader(`./Print/${options.file}.pdf`, groupModel)
+              await unlinkAsync(`./Print/${options.file}.pdf`)
+              groupModel['completedAt'] = this.bringTime()
+              groupModel['status'] = STATUS.STATUS.COMPLETED
+              await this.repository.updateReport(groupModel)
+            } catch (error) {
+              console.log(error)
+            }
           })
-          groupModel['status'] = STATUS.STATUS.UPLOADED
-          groupModel['uploadingCompletedAt'] = this.bringTime()  
-          groupModel['link'] = focc.secure_url 
-          await this.repository.updateReport(groupModel)
-          return focc.secure_url
-        }catch (error) {
-          groupModel['status'] = STATUS.STATUS.FAILED_AT_UPLOADED
-            throw error;
         }
+      })
+      return { "success": true }
+    } catch (error) {
+      throw error;
     }
-    bringTime(){
-        var dt = dateTime.create();
-        var started = dt.format('Y-m-d H:M:S');
-        return started
-    }
-    async printReport(args) {
-        try {  
-            const groupModel = new GroupModel({startedAt:this.bringTime(),status:STATUS.STATUS.STARTED});
-            await this.repository.createReport(groupModel)
-            groupModel['creationStartAt'] = this.bringTime()
-            var html = fs.readFileSync(`./app/Resource/${args.file}.html`, 'utf8');
-            pdf.create(html, options).toStream(async (err, pdfStream) => {
-              if (err) {   
-                console.log(err)
-              } else {
-                groupModel['status'] = STATUS.STATUS.CREATED
-                groupModel['creationCompletedAt'] = this.bringTime()
-                await this.repository.updateReport(groupModel)
-                const path = `./Print/${args.file}.pdf`;
-                const filePath = fs.createWriteStream(path);
-                pdfStream.pipe(filePath);               
-                pdfStream.on('close',async () => {
-                  try{
-                    filePath.close();
-                    let link =  await this.fileuploader(`./Print/${args.file}.pdf`,groupModel) 
-                    await unlinkAsync(`./Print/${args.file}.pdf`)
-                    groupModel['completedAt'] = this.bringTime()
-                    groupModel['status'] = STATUS.STATUS.COMPLETED
-                    await this.repository.updateReport(groupModel)
-                  }catch (error) {
-                    console.log(error)
-                }        
-               })              
-              }
-            })
-            return {"success":true}
-        } catch (error) {
-            throw error;
-        }
-    }
+  }
 }
